c1a4cb633135f8010df468b8150018f7c3f5e255
Raikish <albert.montagut.casero@gmail.com>
2019-01-02 01:52:04 +0100

    formating commentary

--- Yield/Yield.gd
+++ Yield/Yield.gd
@@ -3,10 +3,12 @@ extends Node2D
 signal go
 
 func _ready():
-	_test()# llamamos a la funcion _test
+	# llamamos a la funcion _test
+	_test()
 
 func _test():
-	yield(self, "go") # paramos la ejecucion de la funcion, hasta que se lance la señal
+	# paramos la ejecucion de la funcion, hasta que se lance la señal
+	yield(self, "go")
 	print("GOOOO")
 	_test()
 
