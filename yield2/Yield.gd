fca1773cc9cdd0e855df63f52b3237c10d937a58
Raikish <albert.montagut.casero@gmail.com>
2019-01-24 13:33:02 +0100

    renaming because git don't catch the caps(?)

--- yield2/Yield.gd
+++ yield2/Yield.gd
@@ -2,15 +2,18 @@ extends Node2D
 
 signal go
 
+ 
 func _ready():
 	# llamamos a la funcion _test
 	_test()
+
 
 func _test():
 	# paramos la ejecucion de la funcion, hasta que se lance la señal
 	yield(self, "go")
 	print("GOOOO")
 	_test()
+
 
 func _on_TextureButton_pressed():
 	emit_signal("go")
